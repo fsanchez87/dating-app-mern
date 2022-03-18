6b826f4473744336a81071b01a9d4503b8668ccb
fsanchez87 <sanchezgarridof@gmail.com>
2022-03-18 20:15:51 +0100

    Creating the Swipe Buttons Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
-import DatingCards from './components/DatingCards';
-import Header from './components/Header';
+import "./App.css";
+import DatingCards from "./components/DatingCards";
+import Header from "./components/Header";
+import SwipeButtons from "./components/SwipeButtons";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <DatingCards/>
+      <Header />
+      <DatingCards />
+      <SwipeButtons />
     </div>
   );
 }
